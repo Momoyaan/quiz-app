bfe61addce8fa89bb872a8b215daef5ba653627f
momoyaan <ianmathewaviso@gmail.com>
2023-12-15 19:58:58 +0800

    Hide Headers on some paths

--- src/layout/StudentLayout.tsx
+++ src/layout/StudentLayout.tsx
@@ -1,13 +1,18 @@
 import SideBar from "../components/SideBar";
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 function StudentLayout() {
+  const location = useLocation();
+  const hideHeaderBasePaths = ["/quiz/view", "/quiz/edit", "/quiz/results", "/quiz/answer", "/quiz/result"]; // replace with your base paths
+  const showHeader = !hideHeaderBasePaths.some((basePath) =>
+    location.pathname.startsWith(basePath),
+  );
   return (
     <div className="flex h-screen overflow-hidden">
       <SideBar></SideBar>
       <div className="flex0 relative flex h-full w-full flex-col overflow-y-auto overflow-x-hidden">
-        <Header></Header>
+        {showHeader && <Header></Header>}
         <main>
           <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
             <Outlet></Outlet>
--- src/layout/TeacherLayout.tsx
+++ src/layout/TeacherLayout.tsx
@@ -2,16 +2,17 @@ import SideBar from '../components/SideBar'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom';
 import React from 'react';
-
+import { useLocation } from 'react-router-dom';
 function TeacherLayout() {
-
-
+  const location = useLocation();
+  const hideHeaderBasePaths = ['/quiz/view', '/quiz/edit', '/quiz/results']; // replace with your base paths
+  const showHeader = !hideHeaderBasePaths.some(basePath => location.pathname.startsWith(basePath));
   return (
     <React.Fragment>
     <div className="flex h-screen overflow-hidden">
         <SideBar></SideBar>
         <div className='relative flex h-full w-full flex0 flex-col overflow-y-auto overflow-x-hidden'>
-         <Header></Header> 
+         {showHeader && <Header></Header>} 
         <main>
           <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
             <Outlet></Outlet>
